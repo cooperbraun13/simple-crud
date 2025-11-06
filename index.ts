13c445a16f86323fd4a71c62597bc52128a777fe
cooperbraun13 <cooperjbraun13@gmail.com>
2025-11-05 18:39:44 -0800

    feat: setup basic mongo db connection

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
+import "dotenv/config";
 import express from "express";
 import type { Request, Response } from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(3003, () => {
-  console.log("Server is running on port 3003");
-});
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3003;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI not set, please add it in your .env file");
+}
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Node API");
+  res.send("Hello from Node API Server");
 });
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(PORT, () => {
+      console.log("Server is running on port 3003");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
