36858487867ae60ad9977ac3c3434a20209d328d
cooperbraun13 <cooperjbraun13@gmail.com>
2025-11-11 14:06:44 -0800

    feat: finished apis for getting products and specific product along with adding products to db

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ import "dotenv/config";
 import express from "express";
 import type { Request, Response } from "express";
 import mongoose from "mongoose";
+import { Product } from "./models/product.model";
 
 const app = express();
+app.use(express.json());
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3003;
@@ -15,6 +17,37 @@ if (!MONGODB_URI) {
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Node API Server");
 });
+
+app.get("/api/products", async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: String(error) });
+  }
+});
+
+app.get("/api/product/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: String(error) });
+  }
+});
+
+app.post("/api/products", async (req: Request, res: Response) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: String(error) });
+  }
+});
+
+// update a product
+app.put();
 
 mongoose
   .connect(MONGODB_URI)
--- models/product.model.ts
+++ models/product.model.ts
@@ -29,4 +29,4 @@ const ProductSchema = new mongoose.Schema(
   }
 );
 
-const Product = mongoose.model("Product");
+export const Product = mongoose.model("Product", ProductSchema);
