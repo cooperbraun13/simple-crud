822c0bd6244288e14ad5d92da16ef87405c70a4c
cooperbraun13 <cooperjbraun13@gmail.com>
2025-11-11 14:12:06 -0800

    feat: added update for products and made code more readable with spacing

--- index.ts
+++ index.ts
@@ -21,6 +21,7 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/api/products", async (req: Request, res: Response) => {
   try {
     const products = await Product.find({});
+
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: String(error) });
@@ -30,7 +31,9 @@ app.get("/api/products", async (req: Request, res: Response) => {
 app.get("/api/product/:id", async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
+
     const product = await Product.findById(id);
+
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: String(error) });
@@ -40,6 +43,7 @@ app.get("/api/product/:id", async (req: Request, res: Response) => {
 app.post("/api/products", async (req: Request, res: Response) => {
   try {
     const product = await Product.create(req.body);
+
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: String(error) });
@@ -47,7 +51,22 @@ app.post("/api/products", async (req: Request, res: Response) => {
 });
 
 // update a product
-app.put();
+app.put("api/product/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: String(error) });
+  }
+});
 
 mongoose
   .connect(MONGODB_URI)
